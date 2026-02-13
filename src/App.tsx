2dc682c43fa8b339fa4f57ba6f67c3168cdbfe72
michelemurakami <46617767+michelemurakami@users.noreply.github.com>
2026-02-13 00:27:08 -0800

    Revert " Minigame 1 View Scholarships: Tasks #21 and #22 Displays the character page for minigame 1 and allows the user to select and view scholarship information."

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import MapPage from './pages/MapPage';
 import MinigamePage from './pages/MinigamePage';
-import ScholarshipCharacter from './minigames/minigame1-scholarship/character';
 import './App.css';
 
 function App() {
@@ -12,10 +11,6 @@ function App() {
         <Route path="/" element={<StartPage />} />
         <Route path="/map" element={<MapPage />} />
         <Route path="/minigame/:levelId" element={<MinigamePage />} />
-
-        {/* Page after clicking start for Scholarship mini game */}
-        <Route path="/minigame/level-1/character" element={<ScholarshipCharacter />} />
-        
       </Routes>
     </BrowserRouter>
   );
--- src/minigames/minigame1-scholarship/index.tsx
+++ src/minigames/minigame1-scholarship/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./styles.css";
-import { useNavigate } from 'react-router-dom';
 
 export const metadata = {
   title: "Scholarship Matcher",
@@ -10,39 +9,14 @@ export const metadata = {
 };
 
 const Minigame1: React.FC = () => {
-
-  const navigate = useNavigate();
-
-  // Function to navigate to start of the minigame
-  const handleMinigame1Start = () => {
-    navigate('/minigame/level-1/character');
-  };
-
   return (
-
-    // Placeholder information about how to play the game
     <div className="minigame-level1-container">
-      <h1>Scholarships</h1>
-      <p>
-        Paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-        mollit anim id est laborum
+      <h1 style={{ fontSize: "3rem", marginBottom: "2rem" }}>Hi! I'm Game 1</h1>
+      <p style={{ fontSize: "1.5rem", color: "#666" }}>
+        Scholarship Matcher Module Loaded ✓
       </p>
-
-      {/* navigate to start of the minigame */}
-      <button className="start-minigame1-button" onClick={handleMinigame1Start}>
-        Start game
-      </button>
-
     </div>
   );
 };
 
 export default Minigame1;
-
-
-
